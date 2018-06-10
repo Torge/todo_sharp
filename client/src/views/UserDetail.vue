101fee2561c8886aed1698f0d15c56c44ef7a38e
Torge Jürgensen <torge.juergensen@codeanker.de>
2018-06-10 17:55:15 +0200

    user edit and stuff

--- client/src/views/UserDetail.vue
+++ client/src/views/UserDetail.vue
@@ -1,27 +1,31 @@
 <template>
   <b-card>
-    <h2>Nutzer: {{ users._name }}</h2>
-    <b-btn class="float-right" @click="editUser" >Nutzer Bearbeiten</b-btn>
+    <p>
+      <b>Username:</b> {{ user.username }}
+    </p>
+    <p>
+      <b>Email:</b> {{ user.email }}
+    </p>
+    <b-btn :to="{name: 'user-edit', params: {userId: user._id}}"
+           class="float-right">
+      Nutzer Bearbeiten
+    </b-btn>
   </b-card>
 </template>
 
 <script>
-
 export default {
   name: 'UserDetail',
   computed: {
-    users () {
-      return this.$store.getters['users/current']
+    user () {
+      return this.$store.getters['user/current']
     }
   },
   methods: {
-    editUser () {
-
-    }
+    editUser () {}
   }
 }
 </script>
 
 <style scoped>
-
 </style>
