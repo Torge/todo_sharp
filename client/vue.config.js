4ddd931e3c8433222261d800e11cc2051b7170c0
Torge Jürgensen <torge.juergensen@codeanker.de>
2018-06-11 17:55:58 +0200

    fix

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   configureWebpack: {
     output: {
-      publicPath: process.env.VUE_APP_V === 1 ? '/v1/' : '/v2/'
+      publicPath: process.env.VUE_APP_V == 1 ? '/v1/' : '/v2/'
     }
   }
 }
