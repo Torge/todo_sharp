f77db193f082db33919aef5c1045b4b42e5b9771
Torge Jürgensen <torge.juergensen@codeanker.de>
2018-06-11 11:28:50 +0200

    websocket comments

--- websocket/index.js
+++ websocket/index.js
@@ -2,13 +2,13 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({
   port: 1234
 });
-wss.broadcast = function broadcast(data) {
 
-};
-
+/**
+ * Der Websocket Server leitet die Nachrichten der clients an
+ * jeweils alle anderen clients weiter
+*/
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
     wss.clients.forEach(function each(client) {
       if (client != ws) client.send(message);
     });
